1998e35f53a94440f5851101981fe6c00c5edaf3
EL Mehdi Jessous <elmehdijessous01@gmail.com>
2026-01-02 13:08:52 +0100

    Auth with token

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from .serializers import *
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username = request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"error":"password incorrect"}, status=status.HTTP_404_NOT_FOUND)
+    token ,created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token":token.key, "user":serializer.data},)
+
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username= request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token":token.key,"user":serializer.data},status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication,SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def token_test(request):
+    return Response("passed {}".format(request.user.username))
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication,SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.user.auth_token.delete()
+    try:
+        return Response({"sucessed":"logout sucessed"}, status=status.HTTP_200_OK)
+    except:
+        return Response({"error":"token invaild"}, status=status.HTTP_400_BAD_REQUEST)
--- project/settings.py
+++ project/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'auth_app',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
